b5386298e1c45ba4511f52cd4c3a5de57f90fa67
fusco <emekaignatius5@gmail.com>
2026-03-21 23:04:38 +0100

    fix: fixed the vulnerability issues and fixed build issues

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: process.env.SKIP_LINT === "true",
-  },
   typescript: {
     ignoreBuildErrors: process.env.SKIP_TYPECHECK === "true",
   },
@@ -44,30 +41,6 @@ const nextConfig = {
         ],
       },
     ];
-  },
-
-  webpack: (config, { isServer, nextRuntime }) => {
-    const isEdge = nextRuntime === "edge";
-    if (!isServer || isEdge) {
-      config.resolve = config.resolve || {};
-      config.resolve.alias = {
-        ...(config.resolve.alias || {}),
-
-        // Your server-only files
-        "@/lib/db": false,
-        "@/lib/mail": false,
-        "@/lib/password": false,
-
-        // Node-only deps that often get pulled in transitively
-        resend: false,
-        "@react-email/render": false,
-        "@react-email/components": false,
-        "react-email": false,
-        fs: false,
-        path: false,
-      };
-    }
-    return config;
   },
 };
 
