db2072cd02d76ae8fc543f1d2cd392a7432d3e9d
fusco <emekaignatius5@gmail.com>
2026-04-01 05:40:43 +0100

    fix: remove dangerouslySetInnerHTML for JSON-LD, use script children instead

--- app/(root)/courses/[courseId]/page.tsx
+++ app/(root)/courses/[courseId]/page.tsx
@@ -185,14 +185,14 @@ export default async function CourseSlugPage(props: {
     <div className="min-h-screen bg-background">
       <script
         type="application/ld+json"
-        // nosemgrep: react-dangerouslysetinnerhtml
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(courseJsonLd) }}
-      />
+      >
+        {JSON.stringify(courseJsonLd)}
+      </script>
       <script
         type="application/ld+json"
-        // nosemgrep: react-dangerouslysetinnerhtml
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(breadcrumbJsonLd) }}
-      />
+      >
+        {JSON.stringify(breadcrumbJsonLd)}
+      </script>
       <div className="pt-20">
         <div className="container mx-auto py-8 grid grid-cols-1 lg:grid-cols-3 gap-8">
           <div className="lg:col-span-2 space-y-6">
